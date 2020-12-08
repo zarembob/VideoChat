1e60277bccb23874550334a6092401218f5c1522
Sasha Korniichuk <OKorniichuk03@gmail.com>
2020-12-08 19:45:18 +0200

    Server1

---
+++ ChatServer/DBHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+   public class DBHelper
+    {
+        private ClientModel Context;
+        public void AddContact(Client client)
+        {
+            Context.Clients.Add(client);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ ChatServer/Entity/ClientModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ChatServer
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ClientModel : DbContext
+    {
+   
+        public ClientModel()
+            : base("name=ClientModel")
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+
+    }
+
+   
+}
