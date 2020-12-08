ab1e23308637bd184218750be458867f0aff6e4f
Sasha Korniichuk <OKorniichuk03@gmail.com>
2020-12-08 19:06:44 +0200

    ClassClient

---
+++ ChatServer/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    public class Client
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]      
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
